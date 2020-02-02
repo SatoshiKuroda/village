5dbf366f037b82bbc86d30e198ffdf5fb05bbb88
Satoshi Kuroda <sk@SatoshinoiMac.local>
2020-02-02 15:13:13 +0900

    ちょっとwp化

--- wp-content/themes/onepress-child/header.php
+++ wp-content/themes/onepress-child/header.php
@@ -10,8 +10,18 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
+  <?php do_action( 'onepress_before_site_start' ); ?>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'onepress' ); ?></a>
+    <?php
+    /**
+     * @since 2.0.0
+     */
+    onepress_header();
+    ?>
     <header>
       <nav>
           <?php wp_nav_menu(); ?>
       </nav>
     </header>
+
